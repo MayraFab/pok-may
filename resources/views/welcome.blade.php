d07d30484269e2d968958506a287af7d648f487d
Alumno-ETI <1636534@senati.pe>
2025-11-25 09:38:09 -0500

    Tom x Liz

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Pokédex Laravel</title>
+    <title>Llamocca no olvida a Liz</title>
 
     <style>
         body {
